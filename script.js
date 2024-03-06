2e24765d002c6d0d18b8f8cdba6e27310504b7fe
rafaelvicenzo <rafaellordes.lordes@gmail.com>
2024-03-06 06:19:39 -0300

    firstPart

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function scrollToSection() {
+    const sectionElement = document.querySelector('#profilePicture');
+    if (sectionElement) {
+        const offset = -100;
+        const elementPosition = sectionElement.offsetTop + offset;
+        window.scrollTo({ top: elementPosition, behavior: 'auto' });
+    }
+}
