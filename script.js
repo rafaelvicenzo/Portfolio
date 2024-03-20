d7d5f729e6cec1ce2435b4f3a5d73e8d47dda62e
Rafael Vicenzo <101838564+rafaelvicenzo@users.noreply.github.com>
2024-03-20 15:41:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 document.querySelector('.logoTitle').addEventListener('click', function (event) {
     if (event.currentTarget === event.target) {
-        window.location.href = '/index.html';
+        window.location.href = 'index.html';
     }
 });
 
